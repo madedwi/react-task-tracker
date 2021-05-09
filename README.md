b21dd6f53c40133116b894f4992f0832e38f93ed
I Made Dwi Asmara Putra <putra.work@gmail.com>
2021-05-10 06:29:38 +0800

    Learn how to fetch data from server

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Learning React JS
 
 Source : https://www.youtube.com/watch?v=w7ejDZ8SWv8
+
+## JSON-SERVER
+Add json-server to dev dependencies as mock server for learn fetch data from server
+
+To run mock server: 
+
+`npm run server`
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 import Header from './components/Header'
 import Tasks from './components/Tasks'
@@ -6,28 +6,52 @@ import AddTask from './components/AddTask'
 
 function App() {
 
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            title: 'Task 1',
-            date: '2020-04-01', 
-            reminder: true
-        },
-        {
-            id: 2,
-            title: 'Task 2',
-            date: '2020-04-02', 
-            reminder: true
-        },
-        {
-            id: 3,
-            title: 'Task 3',
-            date: '2020-04-03', 
-            reminder: true
-        }
-    ])
+    const [tasks, setTasks] = useState([])
 
     const [showAddTask, setShowAddTask] = useState(false)
+
+    useEffect(() => {
+        const getTasks = async () => {
+            const tasks = await fetchTasks()
+
+            setTasks(tasks)
+        }
+
+        getTasks()
+
+
+    }, [])
+
+    /**
+     * fetchTasks
+     * 
+     * Get tasks data from mock server
+     * 
+     * @returns array tasks
+     */
+    const fetchTasks = async () => {
+        const req = await fetch('http://localhost:3001/tasks')
+
+        const tasks = await req.json()
+
+        return tasks
+    }
+
+    /**
+     * fetchTask
+     * 
+     * Get specific task data from mock server
+     * 
+     * @param integer id 
+     * @returns object task
+     */
+    const fetchTask = async (id) => {
+        const req = await fetch(`http://localhost:3001/tasks/${id}`)
+
+        const task = await req.json()
+
+        return task
+    }
 
     /**
      * deleteTask
@@ -36,7 +60,12 @@ function App() {
      *
      * @param integer id 
      */
-    const deleteTask = (id) => {
+    const deleteTask = async (id) => {
+
+        await fetch(`http://localhost:3001/tasks/${id}`, {
+            method: 'DELETE'
+        })
+
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
@@ -47,9 +76,22 @@ function App() {
      * 
      * @param integer id 
      */
-    const toggleTask = (id) => {
+    const toggleTask = async (id) => {
+
+        const task = await fetchTask(id)
+
+        const updTask = {...task, reminder: !task.reminder}
+
+        const req = await fetch(`http://localhost:3001/tasks/${id}`, {
+            method: 'PUT',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updTask)
+        })
+
         setTasks(tasks.map((task) => task.id === id 
-            ? {...task, reminder: !task.reminder}
+            ? updTask
             : task ))
     }
 
@@ -62,15 +104,23 @@ function App() {
      * 
      * @param object task 
      */
-    const onAddTask = (task) => {
-        console.log(tasks.map((task) => task.id));
-        const id = Math.max(...(tasks.map((task) => task.id))) + 1;
+    const onAddTask = async (task) => {
+        // console.log(tasks.map((task) => task.id));
+        // const id = Math.max(...(tasks.map((task) => task.id))) + 1;
 
-        task = {...task, id}
+        // task = {...task, id}
 
-        console.log(task);
+        const req = await fetch(`http://localhost:3001/tasks`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
 
-        setTasks([...tasks, task])
+        const newTask = await req.json()
+
+        setTasks([...tasks, newTask])
     }
 
     /**
